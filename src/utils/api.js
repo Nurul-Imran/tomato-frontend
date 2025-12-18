9a27e6410278fafc14487a545c9a5107ff2706ba
Nurul Imran <nurulimran696@gmail.com>
2025-12-18 22:22:18 +0600

    change url in api.js file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://tomato-backend-ux59.onrender.com/api",
 });
 
-export const url = 'http://localhost:4000';
+export const url = 'https://tomato-backend-ux59.onrender.com';
 
 export default api;
