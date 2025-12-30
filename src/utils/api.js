6b5c167e3eb7b284f772c3d96dca3d14a21f61da
Nurul Imran <nurulimran696@gmail.com>
2025-12-31 01:14:03 +0600

    changed url of backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://tomato-frontend-beta.vercel.app/api",
+    baseURL: "https://tomato-backend-ux59.onrender.com/api",
 });
 
-export const url = 'https://tomato-frontend-beta.vercel.app';
+export const url = 'https://tomato-backend-ux59.onrender.com';
 
 export default api;
